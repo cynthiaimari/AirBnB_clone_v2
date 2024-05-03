ff4cfe3abbd0eff7bc87b3d25530115d7e040758
cynthiaimari <cynthiaimari@gmail.com>
2024-05-03 15:00:11 -0700

    AirBnB_clone_v2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.174.244.191', '54.226.21.17']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
